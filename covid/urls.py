463c44149fd45220e9c4c55de09b88c4f097f521
Kirill <k4rt0ff@gmail.com>
2022-01-27 21:14:58 +0100

    django start

--- covid/urls.py
+++ covid/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from covid19.views import sick
 urlpatterns = [
+    path('', sick),
     path('admin/', admin.site.urls),
 ]
--- covid19/views.py
+++ covid19/views.py
@@ -1,3 +1,25 @@
+from django.db.models import Count, Q
+from datetime import datetime
 from django.shortcuts import render
+from .models import SickVaccine
 
-# Create your views here.
+
+def sick(request):
+    # dataset = SickVaccine.objects.values('pozitivni_celkem', 'datum') \
+    #     .annotate(survived_count=Count('pozitivni_celkem', filter=Q(survived=True)),
+    #               not_survived_count=Count('pozitivni_celkem', filter=Q(survived=False))) \
+    #     .order_by('datum')
+    dataset = SickVaccine.objects.values().order_by('datum')
+    date = datetime(2022, 1, 1)
+    dataset2 = SickVaccine.objects.filter(datum__gt=date).values().order_by('datum')
+    return render(request, 'vacine.html', {'dataset': dataset, 'dataset2': dataset2})
+
+
+def sick2022(request):
+    # dataset = SickVaccine.objects.values('pozitivni_celkem', 'datum') \
+    #     .annotate(survived_count=Count('pozitivni_celkem', filter=Q(survived=True)),
+    #               not_survived_count=Count('pozitivni_celkem', filter=Q(survived=False))) \
+    #     .order_by('datum')
+    date = datetime(2022, 1, 1)
+    dataset = SickVaccine.objects.filter(datum__gt=date).values().order_by('datum')
+    return render(request, 'vacine.html', {'dataset2': dataset})

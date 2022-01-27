6cf30a7482d26ab174000e4329666c44ee3dbc19
Kirill <k4rt0ff@gmail.com>
2022-01-27 20:25:45 +0100

    django start

--- covid19/management/commands/import.py
+++ covid19/management/commands/import.py
@@ -2,12 +2,26 @@ from django.core.management.base import BaseCommand
 import pandas as pd
 import requests
 from covid19.models import SickVaccine
+import uuid
+
+
+def update_sick(df):
+    _df = pd.DataFrame.from_records(SickVaccine.objects.all().values())
+    column_name = check_column(SickVaccine, df)
+    if len(_df):
+        _df["clear"] = _df["id"]
+        _df["id"] = _df["id"].astype(str)
+        df = df[column_name].merge(_df, how='left', on=['id'], suffixes=('', '_'))
+        df = df[df.isnull().any(axis=1)]
+
+    obj = (SickVaccine(**vols) for vols in df[column_name].to_dict('records'))
+    SickVaccine.objects.bulk_create(obj)
 
 
 def check_column(model, df):
     column_name = []
     for c_name in df.columns:
-        if c_name != 'id' and c_name in [f.name for f in model._meta.fields] + \
+        if c_name in [f.name for f in model._meta.fields] + \
                 [f'{f.name}_id' for f in model._meta.fields]:
             column_name.append(c_name)
     return column_name
@@ -32,8 +46,6 @@ class Command(BaseCommand):
         file_name = 'ockovani-pozitivni.csv'
         address = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/ockovani-pozitivni.csv"
         get_feed(address, file_name)
-        df = pd.read_csv(file_name)
-        column_name = check_column(SickVaccine, df)
-        obj = (SickVaccine(**vols) for vols in df[column_name].to_dict('records'))
-        SickVaccine.objects.bulk_create(obj)
+        update_sick(pd.read_csv(file_name))
+
         self.stdout.write('Complete')
